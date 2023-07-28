1dc1f31cadf92b94aa82a4a53136b688ea0e4ddb
fgil <f.gil@oechsler.mx>
2023-07-28 11:23:36 -0600

    se crea service de Shift

--- oechsler-back/src/shift/service/shift.service.ts
+++ oechsler-back/src/shift/service/shift.service.ts
@@ -1,25 +1,95 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
+import { Repository, In, Not, IsNull, Like } from "typeorm";
+import { InjectRepository } from "@nestjs/typeorm";
+
 import { CreateShiftDto, UpdateShiftDto } from '../dto/create-shift.dto';
+import { Shift } from "../entities/shift.entity";
 
 @Injectable()
 export class ShiftService {
-  create(createShiftDto: CreateShiftDto) {
-    return 'This action adds a new shift';
+  constructor(
+    @InjectRepository(Shift) private shiftRepository: Repository<Shift>
+  ){}
+
+  async create(createShiftDto: CreateShiftDto) {
+    const shiftExist = await this.shiftRepository.findOne({
+      where: {
+        code: Like(`%${createShiftDto.code}%`)
+      }
+    });
+
+    if (shiftExist?.id) {
+      throw new BadRequestException(`El Turno ya existe`);
+    }
+
+    const shift = this.shiftRepository.create(createShiftDto);
+    return await this.shiftRepository.save(shift);
   }
 
-  findAll() {
-    return `This action returns all shift`;
+  async findAll() {
+    const total = await this.shiftRepository.count();
+    const shifts = await this.shiftRepository.find();
+    
+    if (!shifts) {
+      throw new NotFoundException(`Shifts not found`);
+    }
+    return {
+      total: total,
+      shifts: shifts
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} shift`;
+  async findOne(id: number) {
+    const shift = await this.shiftRepository.findOne({
+      where: {
+        id: id
+      }
+    });
+    if (!shift) {
+      throw new NotFoundException(`Shift #${id} not found`);
+    }
+    return {
+      shift
+    };
   }
 
-  update(id: number, updateShiftDto: UpdateShiftDto) {
-    return `This action updates a #${id} shift`;
+  async update(id: number, updateShiftDto: UpdateShiftDto) {
+    const shift = await this.shiftRepository.findOne({
+      where: {
+        id: id
+      }
+    });
+
+    if (!shift) {
+      throw new NotFoundException(`Shift #${id} not found`);
+    }
+
+    const shiftExist = await this.shiftRepository.findOne({
+      where: {
+        code: Like(`%${updateShiftDto.code}%`),
+        id: Not(id)
+      }
+    });
+
+    if (shiftExist?.id) {
+      throw new BadRequestException(`El Turno ya existe`);
+    }
+
+    const updatedShift = Object.assign(shift, updateShiftDto);
+    return await this.shiftRepository.save(updatedShift);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} shift`;
+  async remove(id: number) {
+    const shift = await this.shiftRepository.findOne({
+      where: {
+        id: id
+      }
+    });
+
+    if (!shift) {
+      throw new NotFoundException(`Shift #${id} not found`);
+    }
+
+    return await this.shiftRepository.softDelete(shift);
   }
 }
