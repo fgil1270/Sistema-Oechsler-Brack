3b2d0a601bde75b593920bdfda0814cdc4398da0
francisco <f.gil@oechsler.mx>
2023-12-22 21:22:36 +0100

    configuracion del servidor

--- oechsler-back/src/app.module.ts
+++ oechsler-back/src/app.module.ts
@@ -46,7 +46,7 @@ import { LogAdjustmentVacationModule } from './log_adjustment_vacation/log_adjus
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '127.0.0.1',
+      host: 'localhost',
       port: 3306,
       username: 'root',
       password: 'root',
