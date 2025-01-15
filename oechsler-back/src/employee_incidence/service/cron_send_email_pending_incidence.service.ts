8af9679303c3a8c37189f447217beeff326f369d
fgil <f.gil@oechsler.mx>
2025-01-14 18:08:56 -0600

    cambios

--- oechsler-back/src/employee_incidence/service/cron_send_email_pending_incidence.service.ts
+++ oechsler-back/src/employee_incidence/service/cron_send_email_pending_incidence.service.ts
@@ -10,28 +10,29 @@ import { EmployeeIncidenceService } from './employee_incidence.service';
 export class CronSendEmailPendingIncidenceService {
     
     constructor(private readonly employeeIncidenceService: EmployeeIncidenceService ) {}
-    //verificar status de solicitud al sat
-    @Cron('*/1 * * * *')
-    solicitudSatXML() {
-        //console.log('Solicitud de descarga de XML cada minuto');
-    }
-
     
+    //*/1 * * * * enviar correo cada minuto
+    //0 */5 * * * * enviar correo cada 5 minutos
     // 0 01 * * * enviar correo cada hora
     //0 0-23/1 * * * enviar correo cada hora
     //0 04 * * * enviar correo cada dia a las 4 am
-    @Cron('0 04 * * *', {
+    @Cron('0 */3 * * * *', {
         timeZone: 'America/Mexico_City',// Especifica la zona horaria de México
         //o si se requiere un offset se puede usar utcOffset
         //utcOffset: '-06:00' // ejemplo para centro de mexico
     })
     async enviarCorreo() {
         const fechaMexico = new Date().toLocaleString('es-MX', { timeZone: 'America/Mexico_City' });
-        await this.employeeIncidenceService.getReportPendingIncidence();
+        
+        if(process.env.PORT == '3501') {
+            
+            await this.employeeIncidenceService.getReportPendingIncidence();
+        }
+        
     }
 
     //0 04 * * * enviar correo cada dia a las 4 am
-    @Cron('0 15 * * *', {
+    @Cron('0 04 * * *', {
         timeZone: 'America/Mexico_City',// Especifica la zona horaria de México
         //o si se requiere un offset se puede usar utcOffset
         //utcOffset: '-06:00' // ejemplo para centro de mexico
@@ -41,15 +42,4 @@ export class CronSendEmailPendingIncidenceService {
         await this.employeeIncidenceService.getReportPendingIncidence48();
     }
 
-    @Cron('0 0 */2 * *')
-    async enviarCorreoCada48Horas() {
-        //console.log('Enviar correo cada 48 horas');
-        /* const subject = 'Correo cada 48 horas';
-        const mailData = {
-        dia: new Date().toISOString(),
-        employeeAutoriza: 'Sistema',
-        };
-        const to = ['recipient@example.com'];
-        await this.mailService.sendEmailCreateIncidence(subject, mailData, to); */
-    }
 }
